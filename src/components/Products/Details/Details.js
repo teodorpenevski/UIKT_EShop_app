88f33da4a2c0136b0c6046ea2cf068e141221b74
Stefan <stefanbt998@outlook.com>
2022-06-26 13:17:11 +0200

    Product details

--- src/components/Products/Details/Details.js
+++ src/components/Products/Details/Details.js
@@ -1,6 +1,8 @@
-import {useParams} from "react-router-dom";
+    import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import EShopService from "../../../repository/EShopRepository";
+
+
 
 const Details =(props)=>{
     const id = useParams();
@@ -21,40 +23,60 @@ const Details =(props)=>{
     }, [id]);
 
     return (
-        <>
-            <h1 className='justify-content-center text-center'>
-                Product details
-            </h1>
-            <div className='row d-flex justify-content-center'>
-                <div className='col-8'>
-                    <div className='row'>
-                        <div className='col-6 text-right'>
-                            PRODUCT NAME
-                        </div>
-                        <div className='col-6'>
-                            {product.name}
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-6 text-right'>
-                            PRODUCT PRICE
-                        </div>
-                        <div className='col-6'>
-                            {product.price}
-                        </div>
-                    </div>
-                    <div className='row'>
-                        <div className='col-6 text-right'>
-                            PRODUCT CATEGORY
-                        </div>
-                        <div className='col-6'>
-                            {product.category.name}
-                        </div>
-                    </div>
-                </div>
+        // <>
+        //     <h1 className='justify-content-center text-center'>
+        //         Product details
+        //     </h1>
+        //     <div className='row d-flex justify-content-center'>
+        //         <div className='col-8'>
+        //             <div className='row'>
+        //                 <div className='col-6 text-right'>
+        //                     PRODUCT NAME
+        //                 </div>
+        //                 <div className='col-6'>
+        //                     {product.name}
+        //                 </div>
+        //             </div>
+        //             <div className='row'>
+        //                 <div className='col-6 text-right'>
+        //                     PRODUCT PRICE
+        //                 </div>
+        //                 <div className='col-6'>
+        //                     {product.price}
+        //                 </div>
+        //             </div>
+        //             <div className='row'>
+        //                 <div className='col-6 text-right'>
+        //                     PRODUCT CATEGORY
+        //                 </div>
+        //                 <div className='col-6'>
+        //                     {product.category.name}
+        //                 </div>
+        //             </div>
+        //         </div>
+        //
+        //     </div>
+        // </>
+        <div className="app">
+            {
+                    <div className="details" key={product.id}>
 
-            </div>
-        </>
+                        <div className="box">
+                            <div className="row">
+                                <h2>Product name:  {product.name}</h2>
+                            </div>
+                            {/*<p>{item.description}</p>*/}
+                            <h2>Product price: ${product.price}</h2>
+                            <h2>Product category: {product.category.name} </h2>
+
+                            {/*<DetailsThumb images={item.src} tab={this.handleTab} myRef={this.myRef} />*/}
+                            <Link className={"btn m-2 btn-primary"} to={"/shoppingcart"}>Add to cart</Link>
+
+                        </div>
+                    </div>
+
+            }
+        </div>
 
     );
 }
